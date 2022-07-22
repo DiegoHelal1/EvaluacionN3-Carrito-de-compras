7b36364981b4f43574d3b500c93a33365532aa8b
DiegoHelal1 <D.Helal.A.A0@gmail.com>
2022-07-22 05:06:27 -0400

    Se agrega Evaluacion

---
+++ src/Data2.js
@@ -0,0 +1,127 @@
+const data = {
+  status: "success",
+  items: [
+    {
+      id: 1,
+      title: "Controlador de motor dc y pap",
+      price: 50,
+      image: "https://www.mechatronicstore.cl/wp-content/uploads/2015/05/r-113.jpg",
+      category: "Switch",
+      cantidad: 1
+    },
+    {
+      id: 2,
+      title: "Filamento PLA SunLu 1kg 1.75mm Colores",
+      price: 35,
+      image: "https://th.bing.com/th/id/OIP.lhX2qrqD5ztmmvUnVwD8hAHaJf?w=152&h=196&c=7&r=0&o=5&dpr=1.1&pid=1.7",
+      category: "Impresion 3D, Filamentos",
+      cantidad: 1
+    },
+    {
+      id: 3,
+      title: "Extractor y succionador de estaño Metalico",
+      price: 335,
+      image: "https://www.mechatronicstore.cl/wp-content/uploads/2015/05/STYLEMARK27022015B.jpg",
+      category: "Herramientas ",
+      cantidad: 1
+    },
+    {
+      id: 4,
+      title: "Modulo regulador de Carga Solar Batería",
+      price: 100,
+      image: "https://www.mechatronicstore.cl/wp-content/uploads/867a1989d6-510x510.webp",
+      category: "Lipo CN3065",
+      cantidad: 1
+    },
+    {
+      id: 5,
+      title: "Conector RJ45 8P8C Cat 5",
+      price: 335,
+      image: "https://www.mechatronicstore.cl/wp-content/uploads/61t8l41Ah-L._AC_SY355_.jpg",
+      category: "Switch",
+      cantidad: 1
+    },
+    {
+      id: 6,
+      title: "Controlador de motor dc y pap",
+      price: 50,
+      image: "https://www.mechatronicstore.cl/wp-content/uploads/2015/05/r-113.jpg",
+      category: "Switch",
+      cantidad: 1
+    },
+    {
+      id: 7,
+      title: "Filamento PLA SunLu 1kg 1.75mm Colores",
+      price: 35,
+      image: "https://th.bing.com/th/id/OIP.lhX2qrqD5ztmmvUnVwD8hAHaJf?w=152&h=196&c=7&r=0&o=5&dpr=1.1&pid=1.7",
+      category: "Impresion 3D, Filamentos",
+      cantidad: 1
+    },
+    {
+      id: 8,
+      title: "Extractor y succionador de estaño Metalico",
+      price: 335,
+      image: "https://www.mechatronicstore.cl/wp-content/uploads/2015/05/STYLEMARK27022015B.jpg",
+      category: "Herramientas ",
+      cantidad: 1
+    },
+    {
+      id: 9,
+      title: "Modulo regulador de Carga Solar Batería",
+      price: 100,
+      image: "https://www.mechatronicstore.cl/wp-content/uploads/867a1989d6-510x510.webp",
+      category: "Lipo CN3065",
+      cantidad: 1
+    },
+    {
+      id: 10,
+      title: "Conector RJ45 8P8C Cat 5",
+      price: 335,
+      image: "https://www.mechatronicstore.cl/wp-content/uploads/61t8l41Ah-L._AC_SY355_.jpg",
+      category: "Switch",
+      cantidad: 1
+    },
+    {
+      id: 11,
+      title: "Controlador de motor dc y pap",
+      price: 50,
+      image: "https://www.mechatronicstore.cl/wp-content/uploads/2015/05/r-113.jpg",
+      category: "Switch",
+      cantidad: 1
+    },
+    {
+      id: 12,
+      title: "Filamento PLA SunLu 1kg 1.75mm Colores",
+      price: 35,
+      image: "https://th.bing.com/th/id/OIP.lhX2qrqD5ztmmvUnVwD8hAHaJf?w=152&h=196&c=7&r=0&o=5&dpr=1.1&pid=1.7",
+      category: "Impresion 3D, Filamentos",
+      cantidad: 1
+    },
+    {
+      id: 13,
+      title: "Extractor y succionador de estaño Metalico",
+      price: 335,
+      image: "https://www.mechatronicstore.cl/wp-content/uploads/2015/05/STYLEMARK27022015B.jpg",
+      category: "Herramientas ",
+      cantidad: 1
+    },
+    {
+      id: 14,
+      title: "Modulo regulador de Carga Solar Batería",
+      price: 100,
+      image: "https://www.mechatronicstore.cl/wp-content/uploads/867a1989d6-510x510.webp",
+      category: "Lipo CN3065",
+      cantidad: 1
+    },
+    {
+      id: 15,
+      title: "Conector RJ45 8P8C Cat 5",
+      price: 335,
+      image: "https://www.mechatronicstore.cl/wp-content/uploads/61t8l41Ah-L._AC_SY355_.jpg",
+      category: "Switch",
+      cantidad: 1
+    },
+  ],
+};
+
+export default data;
